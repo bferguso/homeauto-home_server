fdd8ee837305c76b4f64137522e53acd2451152e
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2021-01-01 16:32:42 -0800

    Make point styles and colours location dependent

--- src/main/html/homeServer/homeServer/static/js/ferginzey.js
+++ src/main/html/homeServer/homeServer/static/js/ferginzey.js
@@ -11,17 +11,38 @@ var toggleVisibility = function(objectId)
     }
 }
 
-var getPointStyle = function(location)
+var getLocationColour = function (location)
 {
     if (location === 'office')
     {
-        return 'circle'
+        return 'rgba(234, 47, 3, 0.6)'
     }
     else if (location === 'bedroom')
     {
+        return 'rgba(70, 132, 70, 0.6)';
+    }
+    else if (location === 'mechanical_room')
+    {
+        return 'rgba(41, 35, 232, 0.6)';
+    }
+    return 'rgba(50,50,50, 0.6)';
+}
+
+var getPointStyle = function(typeOfReading)
+{
+    if (typeOfReading === 'temperature')
+    {
+        return 'circle'
+    }
+    else if (typeOfReading === 'humidity')
+    {
         return 'triangle';
     }
-    return 'rectangle';
+    else if (typeOfReading === 'pressure')
+    {
+        return 'rectRot';
+    }
+    return 'star';
 }
 
 var getLineWidth = function (location)
